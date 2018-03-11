bf812d03b0d6ebd6450e4ad90093162077e23c17
vivianluomin <273104241@qq.com>
2018-03-11 17:29:27 +0800

    第一个游戏

---
+++ RollABall/Assets/Scripts/FlowTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowTarget : MonoBehaviour {
+
+    public Transform playerTransform;
+
+    private Vector3 offest;
+
+	// Use this for initialization
+	void Start () {
+        offest = transform.position - playerTransform.position;
+           
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       transform.position = playerTransform.position + offest;
+	}
+}
---
+++ RollABall/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private Rigidbody rd;
+
+    public int force = 5;
+
+    private int score = 0;
+
+    public Text text;
+
+    public GameObject wintext;
+
+	// Use this for initialization
+	void Start () {
+
+        rd = GetComponent<Rigidbody>();
+        text.text = score.ToString();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rd.AddForce(new Vector3(h,0,v)*force);//给X轴施加一个力
+	}
+
+    //碰撞检测
+    void OnCollisionEnter(Collision collision){
+
+        // collision.collider;//获得碰撞到的游戏物体身上的Collider组件
+       // string name = collision.collider.name;
+        //print(name);
+        if(collision.collider.tag == "pickup")
+        {
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+
+     void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "pickup")
+        {
+            score++;
+            text.text = score.ToString();
+            print(score);
+            Destroy(other.gameObject);
+            if(score == 11)
+            {
+                wintext.SetActive(true);
+            }
+        }
+    }
+
+
+}
