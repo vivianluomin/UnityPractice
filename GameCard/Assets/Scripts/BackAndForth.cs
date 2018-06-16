72ba09ce8270eb2de347864fdc5389a7cd38cd01
vivianluomin <273104241@qq.com>
2018-06-16 17:51:02 +0800

    天空盒和粒子系统

---
+++ GameCard/Assets/Scripts/BackAndForth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth : MonoBehaviour {
+
+    public float speed = 3.0f;
+    public float maxZ = 16.0f;
+    public float minZ = -16.0f;
+
+    private int direction = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, direction * speed * Time.deltaTime);
+        bool bounce = false;
+        if(transform.position.x>maxZ || transform.position.z < minZ){
+            direction = -direction;
+            bounce = true;
+        }
+        if (bounce)
+        {
+            transform.Translate(0, 0, direction * speed * Time.deltaTime);
+        }
+	}
+}
