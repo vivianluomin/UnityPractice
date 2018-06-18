246bd3f7454515cf404121a68f86a24fe0959299
vivianluomin <273104241@qq.com>
2018-06-18 18:50:20 +0800

    FPS练习

---
+++ FPS/Assets/Scripts/GameEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvent : MonoBehaviour {
+
+    public const string ENEMY_HIT = "ENEMY_HIT";
+    public const string SPEED_CHANGED = "SPEED_CHANGED";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ FPS/Assets/Scripts/UIController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    [SerializeField] private Text scoreLabel;
+    [SerializeField] private SettingsPopup settingsPopup;
+
+    private int score;
+
+    void Awake()
+    {
+        Messenger.AddListener(GameEvent.ENEMY_HIT, onEnemyHit);
+    }
+
+    void OnDestroy()
+    {
+        Messenger.RemoveListener(GameEvent.ENEMY_HIT, onEnemyHit);
+    }
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        scoreLabel.text = score.ToString();
+        settingsPopup.Close();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // scoreLabel.text = Time.realtimeSinceStartup.ToString();
+	}
+
+    public void OnOpenSetting()
+    {
+        settingsPopup.Open();
+    }
+
+    private void onEnemyHit()
+    {
+        score++;
+        scoreLabel.text = score.ToString();
+    }
+}
